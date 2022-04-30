88b421baa7961f95328cdd594787f9910782d2ed
Yomna Abdulwahab <59218287+Yomna521@users.noreply.github.com>
2022-05-01 01:47:46 +0200

    Update handler.py

--- service/handler.py
+++ service/handler.py
@@ -18,8 +18,7 @@ class Handler(BaseHTTPRequestHandler):
 
     def do_AUTHHEAD(self):
         self.send_response(401)
-        self.send_header(
-            'WWW-Authenticate', 'Basic realm="Audit Logger"')
+        self.send_header('WWW-Authenticate', 'Basic realm="Audit Logger"')
         self.send_header('Content-type', 'application/json')
         self.end_headers()
 
@@ -54,9 +53,7 @@ class Handler(BaseHTTPRequestHandler):
                     msg_logger = logger.AuditLogger()
                     msg_logger.set_http_request(self.command, self.client_address, query)
                     # save log to database
-
-                    # audit_logs_db.save_log(vars(msg_logger))
-
+                    audit_logs_db.save_log(vars(msg_logger))
                     # delete AuditLogger instance
                     del msg_logger
 
@@ -81,14 +78,14 @@ class Handler(BaseHTTPRequestHandler):
             pass
 
     def do_POST(self):
-        # Check authentication in the request's header
-        # if authentication is sent to none -> send error response
+        # Check authorization in the request's header
+        # if authorization is sent to none -> send error response
         if self.headers.get('Authorization') is None:
             self.do_AUTHHEAD()
             self.wfile.write('No Authentication Header Received'.encode('utf-8'))
             pass
 
-        # if authentication is set with correct key -> process query
+        # if authorization is set with correct key -> process query
         elif self.headers.get('Authorization') == 'Basic ' + str(user.user_key()):
             # extract the event to be logged
             length = int(self.headers.get('Content-Length'))
