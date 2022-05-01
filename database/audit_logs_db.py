4a697af1265d0ab338dd380af6bcbd5c56fa1547
Yomna Abdulwahab <59218287+Yomna521@users.noreply.github.com>
2022-05-01 06:56:39 +0200

    fixing comments

--- database/audit_logs_db.py
+++ database/audit_logs_db.py
@@ -1,10 +1,12 @@
 """"
-    A MongoDB instance to save logs as json entries
+    A MongoDB instance to save and query the audit logs
 """
 import json
 
 from pymongo import MongoClient
 from service import logger
+
+
 # Credentials
 URL = "mongodb://yomna:auditlogger@logs-shard-00-00.5bidw.mongodb.net:27017,logs-shard-00-01.5bidw.mongodb.net:27017,logs-shard-00-02.5bidw.mongodb.net:27017/audit_logs?ssl=true&replicaSet=atlas-kcgm46-shard-0&authSource=admin&retryWrites=true&w=majority"
 log_database = "logs"
